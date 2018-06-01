96fd2436aa1e80246c1fbcf35df01483e582da4c
cristianmarce6 <cristianmarce6@gmail.com>
2018-06-01 01:26:57 -0500

    correcciones y actualización

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include ("facebook-login/autoload.php");
+
+<<<<<<< HEAD
+use Facebook\FacebookSession;
+FacebookSession::setDefaultApplication('1054934174521979', '1054934174521979');
+=======
+FacebookSession::setDefaultApplication('1054934174521979', '9f25916cd25eb96bcf26061364fc6acb');
+>>>>>>> 856736862e10e83e1f015c9e837692847b5a32df
